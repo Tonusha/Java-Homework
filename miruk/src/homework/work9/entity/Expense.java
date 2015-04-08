63a8736df1c916c087ed6888d315483c4e919991
tonusha <Sidtonya@yandex.ru>
2015-04-08 10:25:43 +0300

    Expense

---
+++ miruk/src/homework/work9/entity/Expense.java
@@ -0,0 +1,56 @@
+package homework.work9.entity;
+
+/**
+ * Created by miruk on 08.04.2015.
+ */
+public class Expense {
+    private int num;
+    private String paydate;
+    private int receiver;
+    private int value;
+
+    public Expense() {
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getPaydate() {
+        return paydate;
+    }
+
+    public int getReceiver() {
+        return receiver;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void setPaydate(String paydate) {
+        this.paydate = paydate;
+    }
+
+    public void setReceiver(int receiver) {
+        this.receiver = receiver;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "num=" + num +
+                ", paydate='" + paydate + '\'' +
+                ", receiver=" + receiver +
+                ", value=" + value +
+                '}';
+    }
+}
